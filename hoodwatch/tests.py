e5f75a8dc5a29c48bc162f01040475c7b453c12b
usanase Francoise <fusanasencoise@gmail.com>
2019-11-05 17:57:32 +0200

    model

--- hoodwatch/tests.py
+++ hoodwatch/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 from .models import *
 from django.contrib.auth.models import User
+
+
+class ProfileTest(TestCase):
+    def setUp(self):
+        self.user = User(username = 'fanny', email = 'fanny@fanny.com', password = 'havugima')
+        self.user.save()
+        self.fanny = Profile(bio = 'A python Programmer', user = self.user)
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.fanny,Profile))
+
+
+class HoodTest(TestCase):
+    def setUp(self):
+        self.user = User(username='fanny', email='fanny@fanny.com', password='havugima')
+        self.user.save()
+        self.fanny = Profile(bio='A python Programmer', user=self.user)
+        self.hood = Hood(name = 'jane',bio = "welcome",admin = self.user)
+
+    def tearDown(self):
+        Profile.objects.all().delete()
