0695a16a0e556aa86063722ceb4601146f43cadb
usanase Francoise <fusanasencoise@gmail.com>
2019-11-05 15:28:03 +0200

    model

--- hoodwatch/views.py
+++ hoodwatch/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from hoodwatch.models import Post,Profile,Neighbourhood,Business,Join
+from django.http import HttpResponse
+from .models import Post,Profile,Neighbourhood,Business,Join
+from django.contrib import messages
+from . forms import NewPostForm,UserForm,CreateHoodForm,ProfileForm
+from django.contrib.auth.models import User
 
 # Create your views here.
