61190f0d0d419132e04ce82c933520215e7d653c
usanase Francoise <fusanasencoise@gmail.com>
2019-11-05 18:03:54 +0200

    test

--- hoodwatch/tests.py
+++ hoodwatch/tests.py
@@ -16,12 +16,14 @@ class ProfileTest(TestCase):
         self.assertTrue(isinstance(self.fanny,Profile))
 
 
-class HoodTest(TestCase):
+class PostTest(TestCase):
     def setUp(self):
-        self.user = User(username='fanny', email='fanny@fanny.com', password='havugima')
+        self.user = User(username = 'fanny', email = 'fanny@fanny.com', password = 'havugima')
+        
         self.user.save()
-        self.fanny = Profile(bio='A python Programmer', user=self.user)
-        self.hood = Hood(name = 'jane',bio = "welcome",admin = self.user)
-
-    def tearDown(self):
-        Profile.objects.all().delete()
+        self.kevin = Profile(bio='A python Programmer', user=self.user)
+        self.hood = Hood(name='jane', bio="dukunde", admin=self.user)
+        self.business = Business(name="brian", owner = self.user, business_description= 'hirwa',
+                                 locale = self.hood,business_number = 4322323)
+        self.post = Post(title='Postings',post = 'This is the post',
+                         hood = self.hood, poster = self.user)
