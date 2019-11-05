03d4252dcbb6e277ec714586d188a98f8130d029
usanase Francoise <fusanasencoise@gmail.com>
2019-11-05 15:29:03 +0200

    model

--- hoodwatch/views.py
+++ hoodwatch/views.py
@@ -9,3 +9,34 @@ from . forms import NewPostForm,UserForm,CreateHoodForm,ProfileForm
 from django.contrib.auth.models import User
 
 # Create your views here.
+def index(request):
+    hoods = Neighbourhood.objects.all()
+    business = Business.objects.all()
+    posts = Post.objects.all()
+    print(posts)
+    return render(request, 'index.html',locals())
+
+
+
+@login_required(login_url='/accounts/login/')
+def profile(request,user_id=None):
+    if user_id == None:
+        user_id=request.user.id
+    current_user = User.objects.get(id = user_id)
+    user = current_user
+    images = Post.objects.filter(user=current_user)
+    profile = Profile.objects.all()
+    return render(request, 'profile.html', locals())
+
+
+@login_required(login_url='/accounts/login')
+def updateprofile(request):
+	if request.method == 'POST':
+		form = ProfileForm(request.POST,request.FILES, instance=request.user.profile)
+		if form.is_valid():
+			form.save()
+			return redirect('profile')
+
+	else:
+			form = ProfileForm()
+	return render(request, 'updateprofile.html',{"form":form })
